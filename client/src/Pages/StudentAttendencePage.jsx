d8bb8bd4cc03bf3f0c9d0d1eee02009c0fb02208
hritik-github <hritik2309@gmail.com>
2021-11-15 03:32:19 +0530

    first commit1

---
+++ client/src/Pages/StudentAttendencePage.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {fetchAttendence} from '../redux/action/studentAction'
+import QrReader from 'react-qr-reader'
+import axios from 'axios'
+import HomeHelper from '../Components/HomeHelper'
+import {markAttendence } from '../redux/action/facultyAction'
+import { useHistory } from 'react-router-dom'
+var store1;
+var dispatch1;
+
+const handleError = err => {
+    console.error(err)
+  }
+const Home = () => {
+    const store = useSelector(store => store)
+    store1=store;
+    const dispatch2=useDispatch()
+    dispatch1=dispatch2;
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    
+    useEffect(() => {
+      dispatch(fetchAttendence())  
+    },[])
+
+    return (
+        <div>
+            {store.student.isAuthenticated ? <>
+                <HomeHelper />
+            
+                <div className="container">
+                    
+                    <div className="row mt-5">
+                        <div className="col-md-6 m-auto">
+                            <table className="table border">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">S.No</th>
+                                        <th scope="col">Subject Code</th>
+                                        <th scope="col">Subject Name</th>
+                                        <th scope="col">Maximum Hours</th>
+                                        <th scope="col">Present Hours</th>
+                                        <th scope="col">Absent Hours</th>
+                                        <th scope="col">Total Hours</th>
+                                        <th scope="col">Attendence</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        store.student.attendence.map((res, index) =>
+                                            <tr key={index}>
+                                                <th scope="row">{index + 1}</th>
+                                                <td>{res.subjectCode}</td>
+                                                <td>{res.subjectName}</td>
+                                                <td>{res.maxHours}</td>
+                                                <td>{res.lectureAttended}</td>
+                                                <td>{res.absentHours}</td>
+                                                <td>{res.totalLecturesByFaculty}</td>
+                                                <td>{res.attendence}%</td>
+                                            </tr>
+                                        )
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </> : (history.push('/'))}
+           
+            
+        </div>
+
+    )
+}
+
+export default Home
